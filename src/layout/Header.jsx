199760e8aaa4dec73e45befdbe6124df70faf6e9
Luciana Romero <lucianaromero0512@gmail.com>
2022-07-21 16:23:07 -0300

    add navbar

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -6,17 +6,55 @@ import {
   useColorModeValue,
   Stack,
 } from '@chakra-ui/react'
+import { AiFillFire } from 'react-icons/ai'
+import { FaUserAlt } from 'react-icons/fa'
+
 import { Link } from 'react-router-dom'
 
 const Header = () => {
+  return (
+    <Flex
+      bg={useColorModeValue('white', 'gray.800')}
+      color={useColorModeValue('gray.600', 'white')}
+      minH={'80px'}
+      borderBottom={1}
+      borderStyle={'solid'}
+      borderColor={useColorModeValue('gray.200', 'gray.900')}
+      align={'center'}
+      justifyContent={'space-between'}
+      mx={'5%'}
+    >
+      <Flex>
+        <Link to="/">
+          <Heading>The fairy store ✨</Heading>
+        </Link>
+      </Flex>
+
+      <Flex w={'20%'} flexDirection={'row'} justifyContent={'space-between'}>
+        <Link to="shop">
+          <Text>Shop</Text>
+        </Link>
+        <Link to="aboutUs">
+          <Text>About us</Text>
+        </Link>
+        <Link to="cart">
+          <Text>Cart</Text>
+        </Link>
+        <Text mx="10px" fontSize="1.5rem">
+          <FaUserAlt />
+        </Text>
+      </Flex>
+    </Flex>
+  )
+}
+/* const Header = () => {
   return (
     <Box border={'red solid 1px'}>
       <Flex
         border={'red solid 1px'}
         bg={useColorModeValue('white', 'gray.800')}
         color={useColorModeValue('gray.600', 'white')}
-        minH={'60px'}
-        border={'red solid 1px'}
+        minH={'80px'}
         borderBottom={1}
         borderStyle={'solid'}
         borderColor={useColorModeValue('gray.200', 'gray.900')}
@@ -30,10 +68,8 @@ const Header = () => {
           justifyContent={'space-between'}
           border={'green solid 1px'}
         >
-          <Link to="/" border={'red solid 1px'}>
-            <Text  border={'red solid 1px'}>
-              Home
-            </Text>
+          <Link to="shop" border={'red solid 1px'}>
+            <Text border={'red solid 1px'}>Shop</Text>
           </Link>
           <Link to="aboutUs" border={'red solid 1px'}>
             <Text border={'red solid 1px'}>About us</Text>
@@ -41,13 +77,9 @@ const Header = () => {
           <Link to="cart" border={'red solid 1px'}>
             <Text border={'red solid 1px'}>Cart</Text>
           </Link>
-          <Link to="shop" border={'red solid 1px'}>
-            <Text border={'red solid 1px'}>Shop</Text>
-          </Link>
         </Stack>
       </Flex>
     </Box>
   )
-}
-
+} */
 export default Header
--- src/pages/shop/Shop.jsx
+++ src/pages/shop/Shop.jsx
@@ -7,20 +7,17 @@ const Shop = () => {
   const { data: products, isLoading } = useGet('products')
 
   return (
-    <>
-      <h1>SHOP</h1>
-      <Box w="100%" my="30px">
-        {isLoading && <Spinner mx="50%" my="50px" size="xl" />}
-        {isLoading || (
-          <Grid templateColumns="repeat(4, 1fr)" mx="auto" w="90%">
-            {products &&
-              products.map((product) => {
-                return <ProductCard info={product} key={product.id} />
-              })}
-          </Grid>
-        )}
-      </Box>
-    </>
+    <Box w="100%" my="30px">
+      {isLoading && <Spinner mx="50%" my="50px" size="xl" />}
+      {isLoading || (
+        <Grid templateColumns="repeat(4, 1fr)" mx="auto" w="90%">
+          {products &&
+            products.map((product) => {
+              return <ProductCard info={product} key={product.id} />
+            })}
+        </Grid>
+      )}
+    </Box>
   )
 }
 
