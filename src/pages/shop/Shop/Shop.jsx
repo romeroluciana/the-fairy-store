ed64357b1bdf58023b84fc81c4bde0f36b5c5f25
Luciana Romero <lucianaromero0512@gmail.com>
2022-07-28 17:56:39 -0300

    fix error

--- src/pages/shop/Shop/Shop.jsx
+++ src/pages/shop/Shop/Shop.jsx
@@ -1,16 +1,16 @@
 import { Grid, Box, Spinner, Flex } from '@chakra-ui/react'
 
 import ProductCard from '../../../components/productCard'
-import useGet from '../../../Hooks/useGet'
+import useGet from '../../../hooks/useGet'
 import ProductFilters from './ProductFilters/ProductFilters'
 
 const Shop = () => {
   const { data: products, isLoading } = useGet('products')
 
   return (
-    <Flex m={'5%'} >
+    <Flex m={'5%'}>
       <ProductFilters />
-      <Box w="80%" >
+      <Box w="80%">
         {isLoading && <Spinner mx="50%" my="50px" size="xl" />}
         {isLoading || (
           <Grid templateColumns="repeat(4, 1fr)" mx="auto" w="95%">
